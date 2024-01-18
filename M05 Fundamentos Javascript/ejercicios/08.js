bf6cafe54e30107a2ee5cb22f07dffcea2c0b835
ENRI644 <cre_comercial-094@hotmail.com>
2024-01-18 18:08:15 -0300

    Ejercicios js basicos al 10

--- M05 Fundamentos Javascript/ejercicios/08.js
+++ M05 Fundamentos Javascript/ejercicios/08.js
@@ -3,6 +3,17 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
+
+  console.log("1. " + numero);
+
+  if (numero - Math.ceil(numero) == 0) {
+    console.log("2. " + true)
+    return true;
+  }else{
+    console.log("2. " + false)
+    return false;
+  }
+
 }
 
 module.exports = esNumeroEntero;
