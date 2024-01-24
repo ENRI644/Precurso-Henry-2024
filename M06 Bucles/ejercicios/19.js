b40381102a5c7ab08e4d70e806a561fcd9b61996
ENRI644 <cre_comercial-094@hotmail.com>
2024-01-23 22:39:09 -0300

    ejercicios bucles guia 6

--- M06 Bucles/ejercicios/19.js
+++ M06 Bucles/ejercicios/19.js
@@ -2,6 +2,15 @@ function sumarHastaN(n) {
   // La función recibe un número "n" por argumento.
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Tu código:
+
+  var i = 1;
+  var acu = 0;
+
+  while (i<=n) {
+    acu = acu + i;
+    i++;
+  }
+    return acu;
 }
 
 module.exports = sumarHastaN;
