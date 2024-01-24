368415ea290bd23aa3650d5a7d13da17ff43e284
ENRI644 <cre_comercial-094@hotmail.com>
2024-01-23 22:40:21 -0300

    empezando arreglos

--- M07 Arrays/ejercicios/03.js
+++ M07 Arrays/ejercicios/03.js
@@ -2,6 +2,15 @@ function encontrarElemento(elemento, array) {
   // Busca el elemento pasado por argumento dentro del array.
   // Si no se encuentra, retorna -1.
   // Tu código:
+
+  if (array.length>0) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === elemento) {
+        return i;
+      }else if (i == (array.length-1)) return -1
+    }
+  }else return -1;
+  
 }
 
 module.exports = encontrarElemento;
